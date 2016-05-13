95aac724a11b52e60b1c06c38a50178a97191af9
夏鲁豫 <fiercewind@outlook.com>
2016-05-13 16:00:41 +0800

    添加项目文件。

---
+++ mvvm1/ViewModel/LoginModle.cs
@@ -0,0 +1,40 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace mvvm1.ViewModel
+{
+    class LoginModle:ViewModelBase
+    {
+        private string _username;
+        private string _password;
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                Set(ref _username, value);
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                Set(ref _password, value);
+            }
+        }
+        public ICommand LoginCommand { get; set; }
+
+    }
+}
---
+++ mvvm1/ViewModelLocator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+
+namespace mvvm1
+{
+    class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+        }
+    }
+}

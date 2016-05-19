637c8d28917542d807be6d9f30cd027fdbe475a1
夏鲁豫 <fiercewind@outlook.com>
2016-05-19 13:25:30 +0800

    添加试题页面

---
+++ mvvm1/ViewModel/SingleModel.cs
@@ -0,0 +1,14 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mvvm1.ViewModel
+{
+    class SingleModel: ViewModelBase
+    {
+
+    }
+}
